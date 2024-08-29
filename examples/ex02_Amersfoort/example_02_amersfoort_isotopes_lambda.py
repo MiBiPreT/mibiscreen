fc456b5fab5e5a5ef67b1ee8d43a6a4dbd9879c6
Alraune Zech <a.zech@uu.nl>
2024-08-29 13:24:20 +0200

    linting

--- examples/ex02_Amersfoort/example_02_amersfoort_isotopes_lambda.py
+++ examples/ex02_Amersfoort/example_02_amersfoort_isotopes_lambda.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Lambda plot of isotopes reproducing figure 8 in Paper van Leeuwen et al., 2022
-'Anaerobic degradation of benzene and other aromatic hydrocarbons in a 
+"""Lambda plot of isotopes.
+
+Script reproducing figure 8 in Paper van Leeuwen et al., 2022
+'Anaerobic degradation of benzene and other aromatic hydrocarbons in a
 tar-derived plume: Nitrate versus iron reducing conditions', J. of Cont. Hydrol
 
 data provided on personal basis
@@ -12,24 +13,27 @@ from different data files
 @author: Alraune Zech
 """
 
-import mibipret.data.data as md
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from mibipret.analysis.reduction.stable_isotope_regression import Lambda_regression
+from mibipret.analysis.reduction.stable_isotope_regression import extract_isotope_data
+from mibipret.data.data import load_excel
+from mibipret.data.data import standardize
+from mibipret.visualize.stable_isotope_plots import Lambda_plot
+
+# import sys
+# path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/'
+# sys.path.append(path)
+# from analysis.reduction.stable_isotope_regression import Lambda_regression
+# from analysis.reduction.stable_isotope_regression import extract_isotope_data
+# from visualize.stable_isotope_plots import Lambda_plot
+
 plt.close('all')
-
-# from mibipret.analysis.reduction.stable_isotope_regression import Lambda_regression,extract_isotope_data
-# from mibipret.visualize.stable_isotope_plots import Lambda_plot
-
-import sys
-path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/'
-sys.path.append(path)
-from analysis.reduction.stable_isotope_regression import Lambda_regression,extract_isotope_data
-from visualize.stable_isotope_plots import Lambda_plot
 
 ###------------------------------------------------------------------------###
 ### Script settings
 verbose = True
-molecules = ['benzene','toluene','ethylbenzene','pm_xylene','naphthalene','indene'] 
+molecules = ['benzene','toluene','ethylbenzene','pm_xylene','naphthalene','indene']
 molecules_analysis = []
 ###------------------------------------------------------------------------###
 
@@ -42,30 +46,29 @@ file_path = './amersfoort.xlsx'
 # isotopes_raw,units = md.load_csv(file_path,
 #                                  verbose = verbose)
 
-isotopes_raw,units = md.load_excel(file_path,
+isotopes_raw,units = load_excel(file_path,
                                    sheet_name = 'isotopes',
                                    verbose = verbose)
 
-isotopes,units = md.standardize(isotopes_raw,
+isotopes,units = standardize(isotopes_raw,
                                 reduce = True,
                                 verbose=verbose)
 
 ###------------------------------------------------------------------------###
 ### Lambda regression and Lambda regression plot for all molecules (separately)
 
-for j,molecule in enumerate(molecules): 
+for j,molecule in enumerate(molecules):
 
     C_data,H_data = extract_isotope_data(isotopes,molecule)
 
-    results = Lambda_regression(C_data, 
+    results = Lambda_regression(C_data,
                                 H_data,
                                 validate_indices = True,
                                 verbose = verbose,
                                 )
-    
+
     molecules_analysis.append(results)
 
-    plt.figure(j+1)
     Lambda_plot(**results)#,save_fig = 'Amersfoort_isotope_Lambda_{}.pdf'.format(molecule))
     # Lambda_plot(**results,save_fig = 'Amersfoort_isotope_Lambda_{}.png'.format(molecule),dpi = 300)
 
@@ -77,9 +80,9 @@ fig, axes = plt.subplots(figsize=[7.5,9],
                           ncols = 2,
                           nrows = 3)
 ax = axes.flat
-for j,molecule in enumerate(molecules): 
+for j,molecule in enumerate(molecules):
 
-    results =  molecules_analysis[j]     
+    results =  molecules_analysis[j]
 
     x,y = results['delta_C'], results['delta_H']
 
@@ -89,16 +92,16 @@ for j,molecule in enumerate(molecules):
     ax[j].grid(True,zorder = 0)
 
     # plot trendlines
-    polynomial = np.poly1d(results['coefficients'])           
+    polynomial = np.poly1d(results['coefficients'])
     trendline_x = np.linspace(np.min(x), np.max(x), 50)
     trendline_y = polynomial(trendline_x)
     ax[j].plot(trendline_x, trendline_y, label=r'$\Lambda = {:.0f}$'.format(results['coefficients'][0]))
 
-    if j%2 == 0:        
-        ax[j].set_ylabel('$\delta^2$H')
+    if j%2 == 0:
+        ax[j].set_ylabel(r'$\delta^2$H')
 
-    if j >= len(ax)-2:        
-        ax[j].set_xlabel('$\delta^{{13}}$C')
+    if j >= len(ax)-2:
+        ax[j].set_xlabel(r'$\delta^{{13}}$C')
 
     ax[j].legend()
 
