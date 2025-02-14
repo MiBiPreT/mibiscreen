138329c3fb77661b3d58de581444b9111918a370
Alraune Zech <a.zech@uu.nl>
2025-02-14 10:02:20 +0100

    setup example of ordination and plotting results

--- examples/ex02_Amersfoort/example_02_amersfoort_ordination.py
+++ examples/ex02_Amersfoort/example_02_amersfoort_ordination.py
@@ -7,17 +7,25 @@ Example of diagnostic plotting using ordination with contaminant data from Amers
 
 import pandas as pd
 
-# from mibipret.data.data import load_excel, standardize
+
+from mibipret.data.load_data import load_excel
+from mibipret.data.check_data import standardize, standard_names
+#from mibipret.data.set_data import merge_data, extract_data
+
+# from mibipret.analysis.reduction.transformation import filter_values,transform_values
+from mibipret.analysis.reduction.ordination import pca,cca,rda
+
 from mibipret.visualize.ordination_plot import ordination_plot
-# from mibipret.analysis.reduction.ordination import pca,cca,rda
 
 import sys
-path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/analysis/reduction/'
+path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/analysis/reduction'
 sys.path.append(path) # append the path to module
-from ordination import pca,cca,rda,filter_values,transform_values
-path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/data/'
+from transformation import filter_values,transform_values
+
+path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/data'
 sys.path.append(path) # append the path to module
-from data.data import load_excel, standardize, standard_names, merge_data, extract_data
+from set_data import merge_data, extract_data
+
 
 ###------------------------------------------------------------------------###
 ### Script settings
@@ -31,8 +39,8 @@ file_path = './amersfoort.xlsx'
 ###------------------------------------------------------------------------###
 ### Load and standardize data of environmental quantities/chemicals
 environment_raw,units = load_excel(file_path,
-                                   sheet_name = 'environment',
-                                   verbose = verbose)
+                                    sheet_name = 'environment',
+                                    verbose = verbose)
 
 environment,units = standardize(environment_raw,
                                 reduce = True,
@@ -45,63 +53,60 @@ contaminants_raw,units = load_excel(file_path,
                                     verbose = verbose)
 
 contaminants,units = standardize(contaminants_raw,
-                                 reduce = False,
-                                 verbose=verbose)
+                                  reduce = False,
+                                  verbose = verbose)
 
 
 data = merge_data([environment,contaminants],clean = True)
-variables = ['pH','EC','Redox','DOC','nitrate','nitrite','sulfate','Sum GC',"Mn","Fe"]
+variables_1 = ['pH','EC','Redox','DOC','nitrate','nitrite','sulfate',"Mn","Fe"]
+variables_2 = ['Sum GC']
+# variables = ['pH','EC','Redox','DOC','nitrate','nitrite','sulfate','Sum GC',"Mn","Fe"]
+# variables_standard = standard_names(variables) # standardize names
 # variables_standard = ['pH', 'EC', 'redoxpot', 'DOC', 'nitrate', 'nitrite', 'sulfate', 'manganese', 'iron2']
-variables_standard = standard_names(variables) # standardize names
 
-# extract_data(data,name_list = variables_standard+['sample_nr','obs_well'], inplace = True)
+#display(data)
 extract_data(data,
-             name_list = variables_standard,
+             name_list = variables_1+variables_2,
              keep_setting_data = True,
              inplace = True)
 
-
+#display(data)
 filter_values(data, 
               replace_NaN = 'remove', 
               inplace = True,
               verbose = True)
 
+#display(data)
 transform_values(data,
-                 name_list = ['Sum GC'],
+                 name_list = variables_2,
                  how = 'log_scale',
                  inplace = True,
                  )
 
 transform_values(data,
-                  # name_list = variables_standard,
+                  name_list = variables_1,
                   how = 'standardize',
                   inplace = True,
                 )
 
-data2 = pd.read_csv('data.csv',index_col='well')
-
 ordination_output = pca(data,
-                        independent_variables = variables_standard,
-                         # independent_variables = variables_standard,
-                       # n_comp = 2,
-                        # replace_NaN = 'remove',
-                        # dependent_variables = species,
+                        independent_variables = variables_1+variables_2,
                         verbose = True)
 
-# ordination_output = cca(df,
-#                         independent_variables = environment,
-#                         dependent_variables = species,
-#                   )
+# # ordination_output = cca(df,
+# #                         independent_variables = environment,
+# #                         dependent_variables = species,
+# #                   )
 
-# ordination_output = rda(data,
-#                         independent_variables = environment,
-#                         dependent_variables = species,
-#                   )
-# loadings_independent = ordination_output["loadings_independent"]
-# names_independent = ordination_output["names_independent"]
-# loadings_dependent = ordination_output["loadings_dependent"]
-# names_dependent = ordination_output["names_dependent"]
-# loadings = np.append(loadings_independent, loadings_dependent, axis=0)
+# # ordination_output = rda(data,
+# #                         independent_variables = environment,
+# #                         dependent_variables = species,
+# #                   )
+# # loadings_independent = ordination_output["loadings_independent"]
+# # names_independent = ordination_output["names_independent"]
+# # loadings_dependent = ordination_output["loadings_dependent"]
+# # names_dependent = ordination_output["names_dependent"]
+# # loadings = np.append(loadings_independent, loadings_dependent, axis=0)
 
 fig, ax = ordination_plot(ordination_output=ordination_output,
                 plot_scores = True, 
