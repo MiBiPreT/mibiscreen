6cd184872354a85daeac13c52735fc1138f5888d
Alraune Zech <a.zech@uu.nl>
2025-02-14 14:04:16 +0100

    minors fixes

--- examples/ex02_Amersfoort/example_02_amersfoort_ordination.py
+++ examples/ex02_Amersfoort/example_02_amersfoort_ordination.py
@@ -1,20 +1,17 @@
-"""Ordination plots for Amersfoort data.
+"""Unconstrained Ordination (PCA) with plot for Amersfoort data.
 
 Example of diagnostic plotting using ordination with contaminant data from Amersfoort site.
 
 @author: Alraune Zech
 """
 
-#import pandas as pd
-
-
 from mibipret.data.load_data import load_excel
 from mibipret.data.check_data import standardize, standard_names
 #from mibipret.data.set_data import merge_data, extract_data
 
 # from mibipret.analysis.reduction.transformation import filter_values,transform_values
-from mibipret.analysis.reduction.ordination import pca,cca,rda
-from mibipret.visualize.ordination_plot import ordination_plot
+from mibipret.analysis.reduction.ordination import pca
+# from mibipret.visualize.ordination_plot import ordination_plot
 
 import sys
 path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/analysis/reduction'
@@ -25,6 +22,9 @@ path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/data'
 sys.path.append(path) # append the path to module
 from set_data import merge_data, extract_data
 
+path = '/home/alraune/GitHub/MiBiPreT/mibipret/mibipret/visualize'
+sys.path.append(path) # append the path to module
+from ordination_plot import ordination_plot
 
 ###------------------------------------------------------------------------###
 ### Script settings
@@ -60,61 +60,49 @@ data = merge_data([environment,contaminants],clean = True)
 #display(data)
 
 
+###------------------------------------------------------------------------###
 variables_1 = standard_names(['Sum GC'])
 variables_2 = standard_names(['nitrate','pH','nitrite','sulfate','Redox','EC','DOC',"Mn","Fe"])
 
 
-data_null = extract_data(data,
-             name_list = variables_1 + variables_2,
-             keep_setting_data = True,
-             inplace = True)
+data_ordination = extract_data(data,
+                  name_list = variables_1 + variables_2,
+                  keep_setting_data = True,
+                  )
 
-filter_values(data_null, 
+filter_values(data_ordination, 
               replace_NaN = 'remove', 
               inplace = True,
               verbose = True)
 
-transform_values(data_null,
+transform_values(data_ordination,
                  name_list = variables_1,
                  how = 'log_scale',
                  inplace = True,
                  )
 
-transform_values(data_null,
-                  name_list = variables_1,
+transform_values(data_ordination,
+                 name_list = variables_1,
                   how = 'standardize',
                   inplace = True,
                   )
 
-transform_values(data_null,
-                  name_list = variables_2,
-                  how = 'standardize',
-                  inplace = True,
-                )
+transform_values(data_ordination,
+                 name_list = variables_2,
+                 how = 'standardize',
+                 inplace = True,
+                 )
 
-ordination_output = pca(data_null,
+###------------------------------------------------------------------------###
+ordination_output = pca(data_ordination,
                         independent_variables = variables_1+variables_2,
                         verbose = True)
-
-# # ordination_output = cca(df,
-# #                         independent_variables = environment,
-# #                         dependent_variables = species,
-# #                   )
-
-# # ordination_output = rda(data,
-# #                         independent_variables = environment,
-# #                         dependent_variables = species,
-# #                   )
-# # loadings_independent = ordination_output["loadings_independent"]
-# # names_independent = ordination_output["names_independent"]
-# # loadings_dependent = ordination_output["loadings_dependent"]
-# # names_dependent = ordination_output["names_dependent"]
-# # loadings = np.append(loadings_independent, loadings_dependent, axis=0)
 
 fig, ax = ordination_plot(ordination_output=ordination_output,
                 plot_scores = True, 
                 plot_loadings = True,
                 rescale_loadings_scores = True, 
+                title = "Unconstrained Ordination PCA",
                 # plot_scores = False, 
                 # axis_ranges = [-0.6,0.8,-0.8,1.0],
                 # save_fig = 'save3.png',
