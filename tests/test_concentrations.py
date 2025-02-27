1e731dd08cb211645a3de00a79e31e3c150ddb29
Alraune Zech <a.zech@uu.nl>
2025-02-27 19:52:47 +0100

    linting

--- tests/test_concentrations.py
+++ tests/test_concentrations.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 27 17:22:43 2025
+"""Testing analysis module on NA screening of mibipret.
 
-@author: alraune
+@author: Alraune Zech
 """
 
 import numpy as np
 import pandas as pd
 import pytest
-from mibipret.analysis.sample.concentrations import total_concentration, total_count
+from mibipret.analysis.sample.concentrations import total_concentration
+from mibipret.analysis.sample.concentrations import total_count
+
 
 class Test_total_concentration:
     """Class for testing analysis module on NA screening of mibipret."""
@@ -17,17 +18,16 @@ class Test_total_concentration:
     columns1 = ['sample_nr', 'sulfate', 'benzene']
     s01 = ['2000-001', 748, 263]
     s02 = ['2000-002', 548, 103]
-    
+
     data1 = pd.DataFrame([s01,s02],
                          columns = columns1)
-    
-    
+
+
     def test_total_concentration_01(self):
         """Testing routine total_concentration().
 
         Correct calculation of total amount of contaminants (total concentration).
         """
-   
         out = total_concentration(self.data1).values
         test = self.data1.iloc[:,1:].sum(axis = 1).values
         assert np.all(out == test)
@@ -46,7 +46,7 @@ class Test_total_concentration:
     def test_total_concentration_03(self):
         """Testing routine total_concentration().
 
-        Correct handling when no overlap of specified list of quantities 
+        Correct handling when no overlap of specified list of quantities
         with column names in data frame.
         """
         with pytest.raises(ValueError):
@@ -58,7 +58,6 @@ class Test_total_concentration:
 
         Testing verbose flag.
         """
-
         total_concentration(self.data1,verbose=True)
         out,err=capsys.readouterr()
 
@@ -71,19 +70,17 @@ class Test_total_count:
     columns1 = ['sample_nr', 'sulfate', 'benzene']
     s01 = ['2000-001', 748, 263]
     s02 = ['2000-002', 548, ]
-    
+
     data1 = pd.DataFrame([s01,s02],
                          columns = columns1)
-    
-    
+
     def test_total_count_01(self):
         """Testing routine total_count().
 
         Correct calculation of total count of contaminants with concentration > 0.
         """
-   
         out = total_count(self.data1).values
-        
+
         assert np.all(out == [2,1])
 
     def test_total_count_02(self):
@@ -91,15 +88,14 @@ class Test_total_count:
 
         Correct calculation of total count of contaminants with concentration > specific threshold.
         """
-   
         out = total_count(self.data1,threshold = 300).values
-        
+
         assert np.all(out == [1,1])
 
     def test_total_count_03(self):
         """Testing routine total_count().
 
-        Correct handling when no overlap of specified list of quantities 
+        Correct handling when no overlap of specified list of quantities
         with column names in data frame.
         """
         with pytest.raises(ValueError):
@@ -120,7 +116,7 @@ class Test_total_count:
     def test_total_count_05(self):
         """Testing routine total_count().
 
-        Correct handling when no overlap of specified list of quantities 
+        Correct handling when no overlap of specified list of quantities
         with column names in data frame.
         """
         with pytest.raises(ValueError):
@@ -132,7 +128,6 @@ class Test_total_count:
 
         Testing verbose flag.
         """
-
         total_count(self.data1,verbose=True)
         out,err=capsys.readouterr()
 
