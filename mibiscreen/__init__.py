bb8dfc8703adde6787e72c0ad8fef901ca1deead
raar1 <raarichardson@gmail.com>
2025-05-13 12:21:23 +0200

    Add some data loading methods as top level imports

--- mibiscreen/__init__.py
+++ mibiscreen/__init__.py
@@ -1,8 +1,9 @@
 """Documentation about mibiscreen."""
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Alraune Zech"
 __email__ = "a.zech@uu.nl"
 __version__ = "0.4.0"
+
+
+# Add some commonly used functions as top-level imports
+from mibiscreen.data.load_data import load_excel, load_csv
