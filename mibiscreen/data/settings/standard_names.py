b61779d90a040f95f9a6cdb60149acbc0cc74e17
Alraune Zech <a.zech@uu.nl>
2025-05-20 14:41:32 +0200

    separate file with only the standard names of all quantities

---
+++ mibiscreen/data/settings/standard_names.py
@@ -0,0 +1,108 @@
+"""Name specifications of data!
+
+Listed are all standard names of quantities and parameters measured in
+groundwater samples useful for biodegredation and bioremediation analysis
+
+@author: A. Zech
+"""
+
+### Standard names for settings
+name_sample = "sample_nr"
+name_observation_well = "obs_well"
+name_well_type = "well_type"
+name_sample_depth = "depth"
+name_aquifer = 'aquifer'
+
+### Standard names for environmental parameters
+name_redox = "redoxpot"
+name_pH = "pH"
+name_EC = "EC"
+name_pE = "pE"
+name_NOPC = "NOPC"
+name_DOC = "DOC"
+
+name_oxygen = 'oxygen' #o2
+name_nitrate = 'nitrate' #no3
+name_sulfate = 'sulfate' #"so4"
+name_ironII = "iron2" #"fe_II"
+name_manganese = 'manganese' #"mn_II"
+name_methane = 'methane' #"ch4"
+name_nitrite = 'nitrite' #no2
+name_sulfide = 'sulfide' #"s2min"
+name_ammonium = 'ammonium' #"nh4+"
+name_phosphate = 'phosphate' # "po4"
+name_chloride = 'chloride'
+name_bromide = 'bromide'
+name_fluoride = 'fluoride'
+name_sodium = 'sodium'
+name_magnesium = 'magnesium'
+name_potassium = 'potassium'
+name_calcium = 'calcium'
+name_acetate = 'acetate'
+
+### Standard names for main contaminants
+name_benzene = 'benzene'
+name_toluene = 'toluene'
+name_ethylbenzene = 'ethylbenzene'
+name_pm_xylene = 'pm_xylene'
+name_o_xylene = 'o_xylene'
+name_xylene = 'xylene'
+name_indane = 'indane'
+name_indene = 'indene'
+name_naphthalene = 'naphthalene'
+
+### Standard names for additional contaminants
+name_styrene = 'styrene'
+name_isopropylbenzene = 'isopropylbenzene'
+name_n_propylbenzene = 'n_propylbenzene'
+name_ethyltoluene = 'ethyltoluene'
+name_2_ethyltoluene = '2_ethyltoluene'
+name_3_ethyltoluene = '3_ethyltoluene'
+name_4_ethyltoluene = '4_ethyltoluene'
+name_trimethylbenzene = 'trimethylbenzene'
+name_123_trimethylbenzene = '123_trimethylbenzene'
+name_124_trimethylbenzene = '124_trimethylbenzene'
+name_135_trimethylbenzene = '135_trimethylbenzene'
+name_4_isopropyltouene = '4_isopropyltouene'
+name_13_diethylbenzene = '13_diethylbenzene'
+name_1245_tetramethylbenzene = '1245_tetramethylbenzene'
+name_2_methylindene = '2_methylindene'
+name_1_methylnaphtalene = '1_methylnaphtalene'
+name_2_methylnaphtalene = '2_methylnaphtalene'
+name_2_ethylnaphtalene = '2_ethylnaphtalene'
+name_16_dimethylnaphtalene = '16_dimethylnaphtalene'
+name_26_dimethylnaphtalene = '26_dimethylnaphtalene'
+
+### Standard names for a selection of metabolites
+name_phenol = "phenol"
+name_cinnamic_acid = "cinnamic_acid"
+name_benzoic_acid = "benzoic_acid"
+name_dimethyl_benzoic_acid = 'dimethyl_benzoic_acid'
+name_benzylacetate = 'benzylacetate'
+name_benzoylacetic_acid = "benzoylacetic_acid"
+name_p_coumaric_acid = "p-coumaric_acid"
+name_hydroxycinnamate = "hydroxycinnamate"
+name_acetylphenol = "acetylphenol"
+name_methyl_benzoic_acid = 'methyl_benzoic_acid'
+name_benzylsuccinic_acid = "benzylsuccinic_acid"
+name_3o_toluoyl_propionic_acid = "3o_toluoyl_propionic_acid"
+name_2methylindene = "2_methylindene"
+
+### Standard names for metabolite related quantities
+name_metabolites_conc = "metabolites_concentration"
+name_metabolites_variety = 'metabolites_variety'
+
+### standard names/prefixes for isotopes:
+name_13C = 'delta_13C'
+name_2H = 'delta_2H'
+
+### Standard names for NA screening related quantities
+name_total_oxidators = "total_oxidators"
+name_total_reductors = "total_reductors"
+name_NP_avail = "NP_avail"
+name_e_balance = 'e_balance'
+name_na_traffic_light = 'na_traffic_light'
+name_total_contaminants = "total_contaminants"
+name_intervention_traffic = 'intervention_traffic'
+name_intervention_number = 'intervention_number'
+name_intervention_contaminants = 'intervention_contaminants'
