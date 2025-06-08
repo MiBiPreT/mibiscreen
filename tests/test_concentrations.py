3860adc2f90f8b7e698cc9ae691ed7739fe7f0f0
Alraune Zech <a.zech@uu.nl>
2025-06-08 23:13:49 +0200

    adapt tests to updated function names

--- tests/test_concentrations.py
+++ tests/test_concentrations.py
@@ -8,7 +8,7 @@
 import numpy as np
 import pandas as pd
 import pytest
-from mibiscreen.analysis.sample.concentrations import thresholds_for_intervention
+from mibiscreen.analysis.sample.concentrations import thresholds_for_intervention_traffic
 from mibiscreen.analysis.sample.concentrations import total_concentration
 from mibiscreen.analysis.sample.concentrations import total_contaminant_concentration
 from mibiscreen.analysis.sample.concentrations import total_count
@@ -202,7 +202,7 @@ class TestTotalCount:
         assert len(out)>0
 
 class TestThresholdsForIntervention:
-    """Class for testing thresholds_for_intervention() from module concentation of mibipret."""
+    """Class for testing thresholds_for_intervention_traffic() from module concentation of mibipret."""
 
     data = example_data(with_units = False)
 
@@ -215,90 +215,90 @@ class TestThresholdsForIntervention:
                                     columns = columns)
 
 
-    def test_thresholds_for_intervention_01(self):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_01(self):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Check that routine produced correct dataframe output.
         """
-        na_intervention = thresholds_for_intervention(self.data)
+        na_intervention = thresholds_for_intervention_traffic(self.data)
         intervention_contaminants_cols = ['sample_nr', 'obs_well', 'depth', 'intervention_traffic',
                'intervention_number', 'intervention_contaminants']
 
         assert na_intervention.shape == (4,6)
         assert set(na_intervention.columns) == set(intervention_contaminants_cols)
 
-    def test_thresholds_for_intervention_02(self):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_02(self):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Correct identification of list of contaminants exceeding
         intervention thresholds.
         """
-        na_intervention = thresholds_for_intervention(self.data)
+        na_intervention = thresholds_for_intervention_traffic(self.data)
         intervention_contaminants_test = ['benzene', 'ethylbenzene', 'pm_xylene', 'o_xylene', 'indane', 'naphthalene']
 
         assert set(na_intervention['intervention_contaminants'].iloc[2]) == set(intervention_contaminants_test)
 
-    def test_thresholds_for_intervention_03(self):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_03(self):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Correct identification of number of contaminants exceeding
         intervention thresholds.
         """
-        na_intervention = thresholds_for_intervention(self.data)
+        na_intervention = thresholds_for_intervention_traffic(self.data)
         na_intervention_number_test = 21
         assert (np.sum(na_intervention['intervention_number'].iloc[2]) - na_intervention_number_test)< 1e-5
 
-    def test_thresholds_for_intervention_04(self):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_04(self):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Correct evaluation of traffic light on intervention value.
         """
-        na_intervention = thresholds_for_intervention(self.data)
+        na_intervention = thresholds_for_intervention_traffic(self.data)
         na_intervention_test = ['red','red','red','red']
 
         assert np.all(na_intervention['intervention_traffic'].values == na_intervention_test)
 
-    def test_thresholds_for_intervention_05(self):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_05(self):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Correct handling when unknown group of contaminants are provided.
         """
         with pytest.raises(ValueError):
-            thresholds_for_intervention(self.data,contaminant_group = 'test')
+            thresholds_for_intervention_traffic(self.data,contaminant_group = 'test')
 
-    def test_thresholds_for_intervention_06(self):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_06(self):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Testing Error message that data is not in standard format.
         """
         with pytest.raises(ValueError):
-            thresholds_for_intervention(self.data_nonstandard)
+            thresholds_for_intervention_traffic(self.data_nonstandard)
 
-    def test_thresholds_for_intervention_07(self,capsys):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_07(self,capsys):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Testing Warning that some contaminant concentrations are missing.
         """
         data_test = self.data.drop(labels = 'benzene',axis = 1)
-        thresholds_for_intervention(data_test,verbose = False, contaminant_group='BTEX')
+        thresholds_for_intervention_traffic(data_test,verbose = False, contaminant_group='BTEX')
         out,err=capsys.readouterr()
         assert len(out)>0
 
-    def test_thresholds_for_intervention_08(self):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_08(self):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Testing inplace option adding calculated values as column to data.
         """
         data_test = self.data.copy()
-        thresholds_for_intervention(data_test,include = True)
+        thresholds_for_intervention_traffic(data_test,include = True)
         assert data_test.shape[1] == self.data.shape[1]+3
 
-    def test_thresholds_for_intervention_09(self,capsys):
-        """Testing routine thresholds_for_intervention().
+    def test_thresholds_for_intervention_traffic_09(self,capsys):
+        """Testing routine thresholds_for_intervention_traffic().
 
         Testing verbose flag.
         """
-        thresholds_for_intervention(self.data,verbose=True)
+        thresholds_for_intervention_traffic(self.data,verbose=True)
         out,err=capsys.readouterr()
 
         assert len(out)>0
--- tests/test_screening_plots.py
+++ tests/test_screening_plots.py
@@ -10,7 +10,7 @@ import pytest
 from mibiscreen.data.settings.standard_names import name_metabolites_count
 from mibiscreen.data.settings.standard_names import name_na_traffic_light
 from mibiscreen.data.settings.standard_names import name_total_contaminants
-from mibiscreen.visualize.activity import activity
+from mibiscreen.visualize.screening_plots import activity_plot
 
 
 class TestActivity:
@@ -32,92 +32,92 @@ class TestActivity:
 
     data_04 = [conc,meta,traffic]
 
-    def test_activity_01(self):
-        """Testing routine activity().
+    def test_activity_plot_01(self):
+        """Testing routine activity_plot().
 
         Testing that routine produces a plot when data is provided as DataFrame.
         """
-        fig, ax = activity(self.data_01)
+        fig, ax = activity_plot(self.data_01)
 
         assert isinstance(fig,plt.Figure)
         plt.close(fig)
 
-    def test_activity_02(self):
-        """Testing routine activity().
+    def test_activity_plot_02(self):
+        """Testing routine activity_plot().
 
         Testing that routine produces a plot when data is provided as list of pd.Series
         """
-        fig, ax = activity(self.data_02)
+        fig, ax = activity_plot(self.data_02)
 
         assert isinstance(fig,plt.Figure)
         plt.close(fig)
 
-    def test_activity_03(self):
-        """Testing routine activity().
+    def test_activity_plot_03(self):
+        """Testing routine activity_plot().
 
         Testing that routine produces a plot when data is provided as list of np.arrays
         """
-        fig, ax = activity(self.data_03)
+        fig, ax = activity_plot(self.data_03)
 
         assert isinstance(fig,plt.Figure)
         plt.close(fig)
 
-    def test_activity_04(self):
-        """Testing routine activity().
+    def test_activity_plot_04(self):
+        """Testing routine activity_plot().
 
         Testing that routine produces a plot when data is provided as list of lists
         """
-        fig, ax = activity(self.data_04)
+        fig, ax = activity_plot(self.data_04)
 
         assert isinstance(fig,plt.Figure)
         plt.close(fig)
 
-    def test_activity_05(self):
-        """Testing routine activity().
+    def test_activity_plot_05(self):
+        """Testing routine activity_plot().
 
         Testing Error message that not sufficient data for a plot.
         """
         with pytest.raises(ValueError,
            match="List elements in data must be lists, np.arrays or pd.series."):
-            activity([1,2,3])
+            activity_plot([1,2,3])
 
 
-    def test_activity_06(self):
-        """Testing routine activity().
+    def test_activity_plot_06(self):
+        """Testing routine activity_plot().
 
         Testing Error message that data is not in required format.
         """
         with pytest.raises(ValueError,
            match="Provided arrays/lists/series of data must have the same length."):
-            activity([self.conc,self.meta,self.traffic[:-1]])
+            activity_plot([self.conc,self.meta,self.traffic[:-1]])
 
-    def test_activity_07(self):
-        """Testing routine activity().
+    def test_activity_plot_07(self):
+        """Testing routine activity_plot().
 
         Testing Error message that data is not in required format.
         """
         with pytest.raises(ValueError,
            match="Data needs to be DataFrame or list of at least three lists/np.arrays/pd.series."):
-            activity(self.conc_pd)
+            activity_plot(self.conc_pd)
 
-    def test_activity_08(self):
-        """Testing routine activity().
+    def test_activity_plot_08(self):
+        """Testing routine activity_plot().
 
         Testing Error message that not sufficient data for a plot.
         """
         with pytest.raises(ValueError,
-           match="Too little data for activity plot. At least two values per quantity required."):
-            activity([[1],[2],[3]])
+           match="Too little data for activity_plot plot. At least two values per quantity required."):
+            activity_plot([[1],[2],[3]])
 
-    def test_activity_09(self,capsys):
-        """Testing routine activity().
+    def test_activity_plot_09(self,capsys):
+        """Testing routine activity_plot().
 
         Testing Error message that given file path does not match for writing
         figure to file.
         """
         save_fig = '../dir_does_not_exist/file.png'
         out_text = "WARNING: Figure could not be saved. Check provided file path and name: {}\n".format(save_fig)
-        activity(self.data_01,save_fig = save_fig)
+        activity_plot(self.data_01,save_fig = save_fig)
         out,err=capsys.readouterr()
 
         assert out==out_text
