fe843f3b155e0f850cd81ff8687812e4e623cbe8
Alraune Zech <a.zech@uu.nl>
2025-06-08 14:07:50 +0200

    wrapper functions for calculation of total concentration of contaminants and subgroups and metabolites

--- mibiscreen/analysis/sample/concentrations.py
+++ mibiscreen/analysis/sample/concentrations.py
@@ -83,12 +83,15 @@ def total_concentration(
     ### additing series to data frame
     if include:
         data[name_column] = tot_conc
+        if verbose:
+            print("Series {} saved as column within provided DataFrame".format(name_column))
+            print('---------------------------------------------------------------------------')
 
     return tot_conc
 
 def total_contaminant_concentration(
         data_frame,
-        contaminant_group = "BTEXIIN",
+        contaminant_group = "all_cont",
         include = False,
         verbose = False,
         ):
@@ -100,7 +103,7 @@ def total_contaminant_concentration(
             Contaminant contentrations in [ug/l], i.e. microgram per liter
         contaminant_group: str
             Short name for group of contaminants to use
-            default is 'BTEXIIN' (for benzene, toluene, ethylbenzene, xylene,
+            default is 'all_cont' (for benzene, toluene, ethylbenzene, xylene,
                                   indene, indane and naphthaline)
         include: bool, default False
             Whether to modify the DataFrame rather than creating a new one.
@@ -118,10 +121,56 @@ def total_contaminant_concentration(
         print(" Running function 'total_contaminant_concentration()' on data")
         print('==============================================================')
 
+    if contaminant_group == "all_cont":
+        name_column = names.name_total_contaminants
+    elif contaminant_group == 'BTEX':
+        name_column = names.name_total_BTEX
+    elif contaminant_group == 'BTEXIIN':
+        name_column = names.name_total_BTEXIIN
+    else:
+        name_column = False
+
     tot_conc = total_concentration(
         data_frame,
         name_list = contaminant_group,
-        name_column = names.name_total_contaminants,
+        name_column = name_column,
+        verbose = verbose,
+        include = include,
+        )
+
+    return tot_conc
+
+def total_metabolites_concentration(
+        data_frame,
+        include = False,
+        verbose = False,
+        ):
+    """Function to calculate total concentration of metabolites.
+
+    Input
+    -----
+        data: pd.DataFrame
+            metabolites contentrations in [ug/l], i.e. microgram per liter
+        include: bool, default False
+            Whether to modify the DataFrame rather than creating a new one.
+        verbose: Boolean
+            verbose flag (default False)
+
+    Output
+    ------
+        tot_conc: pd.Series
+            Total concentration of metabolites in [ug/l]
+
+    """
+    if verbose:
+        print('==============================================================')
+        print(" Running function 'total_metabolites_concentration()' on data")
+        print('==============================================================')
+
+    tot_conc = total_concentration(
+        data_frame,
+        name_list = 'all',
+        name_column = names.name_metabolites_conc,
         verbose = verbose,
         include = include,
         )
@@ -131,6 +180,7 @@ def total_contaminant_concentration(
 def total_count(
         data_frame,
         name_list = "all",
+        name_column = False,
         threshold = 0.,
         verbose = False,
         include = False,
@@ -149,6 +199,8 @@ def total_count(
                     - 'BTEXIIN' (for benzene, toluene, ethylbenzene, xylene,
                                   indene, indane and naphthaline)
             or list of strings with names of quantities to use
+        name_column: str or False, default is 'False'
+            optional name of column
         threshold: float, default 0
             threshold concentration value in [ug/l] to test on exceedence
         verbose: Boolean
@@ -183,10 +235,14 @@ def total_count(
     except TypeError:
         raise ValueError("Data not in standardized format. Run 'standardize()' first.")
 
-    if isinstance(name_list, str):
-        name_column = 'total count {}'.format(name_list)
-    elif isinstance(name_list, list):
-        name_column = 'total count selection'
+    if name_column is False:
+        if isinstance(name_list, str):
+            name_column = 'total count {}'.format(name_list)
+        elif isinstance(name_list, list):
+            name_column = 'total count selection'
+    else:
+        if not isinstance(name_column, str):
+            raise ValueError("Keyword 'name_column' needs to be a string or False.")
     total_count.rename(name_column,inplace = True)
 
     if verbose:
@@ -199,6 +255,85 @@ def total_count(
         data[name_column] = total_count
 
     return total_count
+
+def total_contaminant_count(
+        data_frame,
+        include = False,
+        verbose = False,
+        ):
+    """Function to calculate total count of contaminants.
+
+    Input
+    -----
+        data: pd.DataFrame
+            Contaminant contentrations in [ug/l], i.e. microgram per liter
+        contaminant_group: str
+            Short name for group of contaminants to use
+            default is 'all_cont' (for benzene, toluene, ethylbenzene, xylene,
+                                  indene, indane and naphthaline)
+        include: bool, default False
+            Whether to modify the DataFrame rather than creating a new one.
+        verbose: Boolean
+            verbose flag (default False)
+
+    Output
+    ------
+        tot_conc: pd.Series
+            Total count of contaminants in [ug/l]
+
+    """
+    if verbose:
+        print('==============================================================')
+        print(" Running function 'total_contaminant_count()' on data")
+        print('==============================================================')
+
+    tot_conc = total_concentration(
+        data_frame,
+        name_list = "all_cont",
+        name_column = names.name_total_contaminants,
+        verbose = verbose,
+        include = include,
+        )
+
+    return tot_conc
+
+
+def total_metabolites_count(
+        data_frame,
+        include = False,
+        verbose = False,
+        ):
+    """Function to calculate total count of metabolites.
+
+    Input
+    -----
+        data: pd.DataFrame
+            metabolites contentrations in [ug/l], i.e. microgram per liter
+        include: bool, default False
+            Whether to modify the DataFrame rather than creating a new one.
+        verbose: Boolean
+            verbose flag (default False)
+
+    Output
+    ------
+        tot_count: pd.Series
+            Total count of metabolites in [ug/l]
+
+    """
+    if verbose:
+        print('==============================================================')
+        print(" Running function 'total_metabolites_count()' on data")
+        print('==============================================================')
+
+    tot_count = total_count(
+        data_frame,
+        name_list = 'all',
+        name_column = names.name_metabolites_count,
+        verbose = verbose,
+        include = include,
+        )
+
+    return tot_count
 
 def thresholds_for_intervention(
         data_frame,
